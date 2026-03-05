b5e2e87bf298e45053a4eb6fc22d643e6ea4a51e
Pablo Dominguez <pablodominguez@MacBook-Pro-de-Pablo.local>
2026-03-04 19:55:03 -0800

    Update

--- src/controllers/health/health.controller.ts
+++ src/controllers/health/health.controller.ts
@@ -13,7 +13,7 @@ export class HealthController {
       const environment: string = await getServiceEnvironment();
       res.status(200).json({
         success: true,
-        health: 'Up! Grego se la morfa doblada pero igual sape, aunque le guste la japi',
+        health: 'Up! Grego se la morfa doblada pero igual sape, aunque le guste la japi. mentira pero alarga el mensaje. mentira que solo la japi tambien le gustan los travestis',
         version,
         environment,
       });
